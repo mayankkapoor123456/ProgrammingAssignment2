dd9bf37236e6656eaf84dd57a0029a3b584f3558
mayankkapoor123456 <mayank.kapoor@rocketmail.com>
2014-12-22 01:07:41 +0530

    Function for R_Prog Programming Assignment 2
    Contains two new functions as part of the assignment for R_Prog
    coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a list of functions that other functions can call. It uses a matrix as the input data type.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) m <<- solve
+  getSolve <- function() m
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## This function calls the getSolve function and calculates the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ m <- x$getSolve()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setSolve(m)
+    m       ## Return a matrix that is the inverse of 'x'
 }
